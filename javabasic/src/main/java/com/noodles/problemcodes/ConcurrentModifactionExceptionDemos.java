90ed1f79e6e370df3059c5e7f71b659f64e15fbb
Noodles <645465550@qq.com>
2019-04-21 20:56:48 +0800

    新增错误代码包，记录一些常见的错误写法的代码

---
+++ javabasic/src/main/java/com/noodles/problemcodes/ConcurrentModifactionExceptionDemos.java
@@ -0,0 +1,75 @@
+package com.noodles.problemcodes;
+
+import com.noodles.gson.JsonUtil;
+import jdk.jfr.Description;
+import org.openjdk.jmh.annotations.Param;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @program: noodles-projects
+ * @description: 遍历集合时同时对集合进行修改，抛出异常
+ * @author: Eric
+ * @create: 2019-04-21 19:51
+ **/
+public class ConcurrentModifactionExceptionDemos {
+
+
+    public static void main(String args[]){
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+
+        //迭代器去除元素
+        rightCode(integerList, (i) -> i == 2);
+        System.out.println(JsonUtil.toJson(integerList));
+
+
+
+
+        /**抛出ConcurrentModificationException*/
+        //wrongCode(integerList, (i)-> i == 2);
+
+
+
+    }
+
+  
+    /** 
+    * @Description:  错误写法，在遍历List过程中，符合某个条件时，移除元素
+    * @Param:  
+    * @return:  
+    * @Author: Eric
+    * @Date: 2019-04-21 
+    */ 
+    public static <T> void wrongCode(List<T> list, Predicate<T> p){
+        for(T e : list){
+            if(p.test(e)){
+                list.remove(e);
+            }
+        }
+    }
+
+
+    /**
+    * @Description: 正确写法，用迭代器
+    * @Param:
+    * @return:
+    * @Author: Eric
+    * @Date: 2019-04-21
+    */
+    public static <T> void rightCode(List<T> list, Predicate<T> p){
+
+        Iterator<T> iterator = list.iterator();
+        while(iterator.hasNext()){
+            if(p.test(iterator.next())){
+                iterator.remove();
+            }
+        }
+
+    }
+}
