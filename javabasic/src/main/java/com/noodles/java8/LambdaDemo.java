53fab554212cf48311c7fdab8faee4fbb1c03975
Lid23 <645465550@qq.com>
2019-08-30 17:39:00 +0800

    Java8代码整理

--- javabasic/src/main/java/com/noodles/java8/LambdaDemo.java
+++ javabasic/src/main/java/com/noodles/java8/LambdaDemo.java
@@ -1,6 +1,13 @@
 package com.noodles.java8;
 
+import java.security.PrivilegedAction;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.concurrent.Callable;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import com.noodles.java8.bean.Apple;
 
@@ -12,8 +19,29 @@ import com.noodles.java8.bean.Apple;
  */
 public class LambdaDemo {
 
-	/**Comparator*/
-	Comparator<Apple> appleWeightComparator = (a1, a2) -> a1.getWeight().compareTo(a2.getWeight());
+	public static void main(String[] args) {
 
-	Runnable runnable = () -> System.out.println();
+	/**Comparator*/
+		Comparator<Apple> appleWeightComparator = (a1, a2) -> a1.getWeight().compareTo(a2.getWeight());
+
+		Runnable runnable = () -> System.out.println();
+
+		/**同样的Lambda，不同的函数式接口*/
+		Callable<Integer> callable = () -> 42;
+		PrivilegedAction<Integer> privilegedAction = () -> 42;
+
+		/**Predicate返回了一个boolean*/
+		Predicate<String> p = s -> new ArrayList().add(s);
+		/**void兼容：Consumer返回了一个void*/
+		Consumer<String> b = s -> new ArrayList().add(s);
+
+		/**构造函数引用*/
+		Supplier<Apple> appleSupplier = Apple::new;
+		Apple apple = appleSupplier.get();
+
+		BiFunction<Integer, String, Apple> appleBiFunction = Apple::new;
+		System.out.println(appleBiFunction.apply(100, "red"));
+	}
+
+
 }
