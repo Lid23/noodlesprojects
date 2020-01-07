c5eda52e8f9f2505ac46434d34f773a3c39863f1
Lid23 <645465550@qq.com>
2020-01-07 18:29:03 +0800

    Java新增对象实例化示例

---
+++ javabasic/src/main/java/com/noodles/javabasis/classtest/ObjectInstance.java
@@ -0,0 +1,13 @@
+package com.noodles.javabasis.classtest;
+
+/**
+ * @filename ObjectInstance
+ * @description 对象实例化示例
+ * @author 巫威
+ * @date 2020/1/7 18:27
+ */
+public class ObjectInstance {
+
+	/**查看字节码指令命令javap -verbose -p ObjectInstantiate.class*/
+	Object object = new Object();
+}
